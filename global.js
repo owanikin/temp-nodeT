da6bf34c1643ff41922d28f756ad8c2402c461c2
Test <test.@As-MacBook-Pro.local>
2021-05-03 21:16:59 +0100

    first node commit

---
+++ global.js
@@ -0,0 +1,9 @@
+const amount = 8
+
+if (amount < 10) {
+    console.log('It is a small number')
+} else {
+    console.log('it is a large number')
+}
+
+console.log(`hey, this is my first node app!!!`)
---
+++ modules.js
@@ -0,0 +1,9 @@
+const names = require('./names')
+const sayHi = require('./utils')
+const data = require('./alternative-flavor')
+require('./mind-grenade')
+
+// console.log(data)
+// sayHi('Susan')
+// sayHi(names.john)
+// sayHi(names.peter)
